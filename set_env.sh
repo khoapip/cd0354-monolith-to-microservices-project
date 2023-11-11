3261062bb4a96ccb59c30c4caef26d41d03eee8e
khoapip <ngotheanhkhoa@gmail.com>
2023-11-11 14:23:01 +0700

    submit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=projec3-db.c8s8w4dggqgx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=projec3-db
+export AWS_BUCKET=p3-khoangothe
+export AWS_REGION=us-east-1
+export AWS_PROFILE=project3
+export JWT_SECRET=testing
+export URL=http://localhost:8100
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  //apiHost: 'http://ad880582ce23b490c9f65a7acbdabe75-144750587.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost:'http://a22d1150a842841239aa4bcf1af22f84-1183227908.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
